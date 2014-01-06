6534562b7e4152aba2f7c1a4a5ea93cc5941d48e
Dirk Groenen <dirk@bitlabs.nl>
2014-01-06 15:16:36 +0100

    Added a callback function

--- viewportchecker.js
+++ viewportchecker.js
@@ -1,18 +1,18 @@
 /*
-Version 1.2
-The MIT License (MIT)
+    Version 1.3
+    The MIT License (MIT)
 
-Copyright (c) 2014 Dirk Groenen
+    Copyright (c) 2014 Dirk Groenen
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 */
 
 (function($){
@@ -20,33 +20,48 @@ copies or substantial portions of the Software.
         // Define options and extend with user
         var options = {
             classToAdd: 'visible',
-            offset: 100
+            offset: 100,
+            callbackFunction: function(){}
         }
         $.extend(options, useroptions);
 
+        // Cache the given element and height of the browser
         var $elem = this;
+        var windowHeight = $(window).height();
+
         this.checkElements = function(){
-            // Cache given element into $elem and set some variables
+            // Set some vars to check with
             var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html'),
                 viewportTop = $(scrollElem).scrollTop(),
-                viewportBottom = ( viewportTop + $(window).height() );
+                viewportBottom = (viewportTop + windowHeight);
 
             $elem.each(function(){
+                var $obj = $(this);
                 // If class already exists; quit
-                if ($(this).hasClass(options.classToAdd))
+                if ($obj.hasClass(options.classToAdd))
                     return;
 
                 // define the top position of the element and include the offset which makes is appear earlier or later
-                var elemTop = Math.round( $(this).offset().top ) + options.offset,
-                    elemBottom = elemTop + ($(this).height());
+                var elemTop = Math.round( $obj.offset().top ) + options.offset,
+                    elemBottom = elemTop + ($obj.height());
 
                 // Add class if in viewport
-                if ((elemTop < viewportBottom) && (elemBottom > viewportTop))
-                    $(this).addClass(options.classToAdd);
+                if ((elemTop < viewportBottom) && (elemBottom > viewportTop)){
+                    $obj.addClass(options.classToAdd);
+
+                    // Do the callback function. Callback wil send the jQuery object as parameter
+                    options.callbackFunction($obj);
+                }
             });
         }
 
+        // Run checkelements on load and scroll
         $(window).scroll(this.checkElements);
         this.checkElements()
+
+        // On resize change the height var
+        $(window).resize(function(e){
+            windowHeight = e.currentTarget.innerHeight
+        });
     }
 })(jQuery);
