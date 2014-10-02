1ce86b3179936aedbd275a4b54301e306088ecd7
Dirk Groenen <dirk@bitlabs.nl>
2014-10-02 10:50:20 +0200

    Added support for horizontal scrolling

--- jquery.viewportchecker.js
+++ jquery.viewportchecker.js
@@ -1,5 +1,5 @@
 /*
-    Version 1.4.2
+    Version 1.5.0
     The MIT License (MIT)
 
     Copyright (c) 2014 Dirk Groenen
@@ -22,20 +22,28 @@
             classToAdd: 'visible',
             offset: 100,
             repeat: false,
-            callbackFunction: function(elem, action){}
+            callbackFunction: function(elem, action){},
+			scrollHorizontal: false
         };
         $.extend(options, useroptions);
 
         // Cache the given element and height of the browser
         var $elem = this,
-            windowHeight = $(window).height(),
+            windowSize = (!options.scrollHorizontal) ? $(window).height() : $(window).width(),
             scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
 
         this.checkElements = function(){
         
             // Set some vars to check with
-            var viewportTop = $(scrollElem).scrollTop(),
-                viewportBottom = (viewportTop + windowHeight);
+			if(!options.scrollHorizontal){
+				var viewportTop = $(scrollElem).scrollTop(),
+					viewportBottom = (viewportTop + windowSize);
+			}
+			else{
+				var viewportTop = $(scrollElem).scrollLeft(),
+					viewportBottom = (viewportTop + windowSize);
+			}
+            
 
             $elem.each(function(){
                 var $obj = $(this);
@@ -45,7 +53,7 @@
                 }
 
                 // define the top position of the element and include the offset which makes is appear earlier or later
-                var elemTop = Math.round( $obj.offset().top ) + options.offset,
+                var elemTop = (!options.scrollHorizontal) ? Math.round( $obj.offset().top ) + options.offset : Math.round( $obj.offset().left ) + options.offset,
                     elemBottom = elemTop + ($obj.height());
 
                 // Add class if in viewport
@@ -71,7 +79,7 @@
 
         // On resize change the height var
         $(window).resize(function(e){
-            windowHeight = e.currentTarget.innerHeight;
+            windowSize = (!options.scrollHorizontal) ? e.currentTarget.innerHeight : e.currentTarget.innerWidth;
         });
         
         // trigger inital check if elements already visible
